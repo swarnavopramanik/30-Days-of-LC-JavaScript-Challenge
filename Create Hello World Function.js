e6974d0c8db3eef9521970c7285207ab97d624f0
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-05-05 15:37:47 +0530

    Update Create Hello World Function.js

--- Create Hello World Function.js
+++ Create Hello World Function.js
@@ -1,1 +1,39 @@
 
+Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+ 
+
+Example 1:
+
+Input: args = []
+Output: "Hello World"
+Explanation:
+const f = createHelloWorld();
+f(); // "Hello World"
+
+The function returned by createHelloWorld should always return "Hello World".
+Example 2:
+
+Input: args = [{},null,42]
+Output: "Hello World"
+Explanation:
+const f = createHelloWorld();
+f({}, null, 42); // "Hello World"
+
+Any arguments could be passed to the function but it should still always return "Hello World".
+
+// Solution 
+
+function createHelloWorld() {
+  const greeting = "Hello World";
+  
+  return function() {
+    return greeting;
+  };
+} 
+
+
+
+/* var createHelloWorld = function() {
+        return () => "Hello World";
+}; */
+
